b86db3a59371763cc38176bef99a9268832fb86e
Crowdbotics <team@crowdbotics.com>
2021-08-28 12:27:46 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "ref_watch_30160Identifier",
+  url: "https://ref_watch_30160Identifier.botics.co",
+  api: "https://ref_watch_30160Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
